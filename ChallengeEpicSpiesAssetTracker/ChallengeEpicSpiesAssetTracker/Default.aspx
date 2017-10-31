53d8a2aca8ec5b7396f1d4acb42c9519b1d887a1
jroper1130 <warbeast78@hotmail.com>
2017-10-31 02:52:16 -0700

    Add files via upload

---
+++ ChallengeEpicSpiesAssetTracker/ChallengeEpicSpiesAssetTracker/Default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeEpicSpiesAssetTracker.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 190px;
+            height: 190px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <img alt="" class="auto-style1" src="download.png" /><br />
+            <br />
+            <strong>Asset Performance Tracker</strong><br />
+            <br />
+            Asset Name: <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Elections Rigged:
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Acts of Subterfuge Performed:
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Add Asset" />
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeEpicSpiesAssetTracker/ChallengeEpicSpiesAssetTracker/Default.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssetTracker
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                //initialize 3 arrays
+                string[] assets = new string[0];
+                int[] elections = new int[0];
+                int[] acts = new int[0];
+                //setup viewstate for arrays
+                ViewState.Add("Assets", assets);
+                ViewState.Add("Elections", elections);
+                ViewState.Add("Acts", acts);
+            }
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //link arrays to viewstate to record their data
+            string[] assets = (string[])ViewState["Assets"];
+            int[] elections = (int[])ViewState["Elections"];
+            int[] acts = (int[])ViewState["Acts"];
+            // this index helps makes new arrays through resize method
+            int newLength = assets.Length + 1;
+            Array.Resize(ref assets, newLength);
+            Array.Resize(ref elections, newLength);
+            Array.Resize(ref acts, newLength);
+            //this index helps link arrays to accept text box values
+            int newIndex = assets.GetUpperBound(0); 
+            assets[newIndex] = TextBox1.Text;
+            elections[newIndex] = int.Parse(TextBox2.Text);
+            acts[newIndex] = int.Parse(TextBox3.Text);
+            //link viewstate to arrays
+            ViewState["Assets"] = assets;
+            ViewState["Elections"] = elections;
+            ViewState["Acts"] = acts;
+            //record in formatted string
+            Label1.Text = String.Format("Total Elections Rigged: {0}" +
+                "<br />Average Acts of Subterfuge: {1}" +
+                "<br />Last Asset you Added: {2}",
+                elections.Sum(),
+                acts.Average(), 
+                assets[newIndex]);
+            //clear out text boxes
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+            
+
+        }
+    }
+}
